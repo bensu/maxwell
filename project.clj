af929663e4756847a97cf8c53ad13381e536f217
Sebastian Bensusan <sbensu@gmail.com>
2015-05-06 16:26:25 +0200

    WIP: updated README and moved deps around

--- project.clj
+++ project.clj
@@ -1,25 +1,23 @@
 (defproject maxwell "0.1.0-SNAPSHOT"
   :description "Minimal Wrapper around goog.userAgent to report stacktraces"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/bensu/maxwell"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0-beta2" :scope "provided"]
                  [org.clojure/clojurescript "0.0-3211" :scope "provided"]] 
 
-  :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-figwheel "0.3.1"]]
-
   :source-paths ["src"]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
   
+  :profiles {:dev {:plugins [[lein-cljsbuild "1.0.5"]
+                             [lein-figwheel "0.3.1"]]}}
+
   :cljsbuild 
   {:builds [{:id "dev"
              :source-paths ["src"]
-              
              :figwheel :true 
-
              :compiler {:main maxwell.smart
                         :asset-path "js/compiled/out"
                         :output-to "resources/public/js/compiled/maxwell.js"
