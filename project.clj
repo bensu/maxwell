9f52aa5775682de6219fcd239f794c9727d927a4
Sebastian Bensusan <sbensu@gmail.com>
2015-05-08 23:13:48 +0200

    Added clojars info

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
 (defproject maxwell "0.1.0-SNAPSHOT"
-  :description "Minimal Wrapper around goog.userAgent to report stacktraces"
+  :description "Minimal wrapper around goog.library to log client info & errors"
   :url "https://github.com/bensu/maxwell"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :scm {:name "git"
+        :url "https://github.com/bensu/maxwell"}
+
+  :deploy-repositories [["clojars" {:creds :gpg}]]
 
   :dependencies [[org.clojure/clojure "1.7.0-beta2" :scope "provided"]
                  [org.clojure/clojurescript "0.0-3255" :scope "provided"]] 
